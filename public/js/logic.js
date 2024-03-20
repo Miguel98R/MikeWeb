479672d1a3559040f59e6fb3acbc25856b201ffd
Miguel Rosas <111320389+mikeleganux1998@users.noreply.github.com>
2024-03-19 20:47:10 -0600

    update 2024

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,16 +1,10 @@
 $(function () {
     $("#download_cv").click(function () {
-        // Ruta al archivo en la carpeta "docs"
-        const filePath = '/public/docs/CV_JoseMiguelRosas.pdf'; // Reemplaza 'nombre_del_archivo.pdf' con el nombre de tu archivo
-
-        // Crear un enlace temporal para la descarga
+        const filePath = '/public/docs/CV_JoseMiguelRosas.pdf';
         var a = document.createElement('a');
         a.href = filePath;
+        a.download = 'CV_JoseMiguelRosas.pdf';
 
-        // Establecer el atributo "download" para que el navegador inicie la descarga
-        a.download = 'CV_JoseMiguelRosas.pdf'; // Reemplaza 'nombre_del_archivo.pdf' con el nombre de tu archivo
-
-        // Simular un clic en el enlace para iniciar la descarga
         a.click();
     });
 });
